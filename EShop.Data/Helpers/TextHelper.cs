9dc2344f7805c145af940366f86cab41ce3afd1f
BohdanMolchanov <molchanovbohdan@gmail.com>
2021-04-14 22:41:01 +0300

    Product form updated

--- EShop.Data/Helpers/TextHelper.cs
+++ EShop.Data/Helpers/TextHelper.cs
@@ -24,8 +24,8 @@ namespace EShop.Data.Helpers
                $"{(productEntity.DisplayDiagonal.HasValue ? $"Екран: {productEntity.DisplayDiagonal}\'\' {dotChar} " : "")}" +
                $"{(productEntity.DisplayType.HasValue ? $"{productEntity.DisplayType} {dotChar} " : "")}" +
                $"{(productEntity.DisplayWidth.HasValue && productEntity.DisplayHeight.HasValue ? $"{productEntity.DisplayWidth}x{productEntity.DisplayHeight} {dotChar} " : "")}" +
-               $"{(productEntity.Rom.HasValue ? $"Внутрішня пам'ять: {productEntity.Rom} {dotChar} " : "")}" +
-               $"{(productEntity.Ram.HasValue ? $"Оперативна пам'ять: {productEntity.Ram} {dotChar} " : "")}" +
+               $"{(productEntity.Rom.HasValue ? $"Внутрішня пам'ять: {productEntity.Rom} ГБ {dotChar} " : "")}" +
+               $"{(productEntity.Ram.HasValue ? $"Оперативна пам'ять: {productEntity.Ram} ГБ {dotChar} " : "")}" +
                $"{(!string.IsNullOrEmpty(productEntity.ProcessorName) ? $"Процесор: {productEntity.ProcessorName} {dotChar} " : "")}" +
                $"{(productEntity.NumberOfCores.HasValue ? $"Кількість ядер: {productEntity.NumberOfCores} {dotChar} " : "")}" +
                $"{(productEntity.Clock.HasValue ? $"Частота, GHz: {productEntity.Clock} {dotChar} " : "")}" +
@@ -36,10 +36,32 @@ namespace EShop.Data.Helpers
                $"{(productEntity.Width.HasValue ? $"Ширина корпусу, мм: {productEntity.Width} {dotChar} " : "")}" +
                $"{(productEntity.Height.HasValue ? $"Висота корпусу, мм: {productEntity.Height} {dotChar} " : "")}" +
                $"{(productEntity.Weight.HasValue ? $"Вага корпусу, г: {productEntity.Weight} {dotChar} " : "")}" +
-               $"{(productEntity.HasSdCardSlot ? $"Присутній лоток для сім-карти {dotChar} " : $"Лоток для сім-карти відсутній {dotChar} ")}" +
+               $"{(productEntity.HasSdCardSlot ? $"Присутній лоток для SD-карти {dotChar} " : $"Лоток для SD-карти відсутній {dotChar} ")}" +
                $"{(productEntity.HasNFC ? $"Присутній модуль NFC" : $"Модуль NFC відсутній")}"
                 ;
         }
+        public static Dictionary<string, string> PrepareSpecifications(ProductEntity pe) => new Dictionary<string, string>()
+        {
+            {"Виробник:", string.IsNullOrEmpty(pe.Manufacturer) ? "" : pe.Manufacturer},
+            {"ОС:",!string.IsNullOrEmpty(pe.OperatingSystem) ? pe.OperatingSystem : "-"},
+            {"Кількість сім-карток:", pe.NumberOfSimCards.HasValue ? pe.NumberOfSimCards.ToString() : "-"},
+            {"NFC",pe.HasNFC ? "+" : "-"},
+            {"Можливість встановлення SD",pe.HasSdCardSlot ? "+" : "-"},
+            {"Тип дисплею:", pe.DisplayType.HasValue ? pe.DisplayType.ToString() : "-"},
+            {"Діагональ дисплею:", pe.DisplayDiagonal.HasValue ? $"{pe.DisplayDiagonal}''" : "-"},
+            {"Роздільна здатність дисплею:", pe.DisplayWidth.HasValue && pe.DisplayHeight.HasValue ? $"{pe.DisplayWidth}x{pe.DisplayHeight}" : "-" },
+            {"Процесор:",!string.IsNullOrEmpty(pe.ProcessorName) ? pe.ProcessorName : "-"},
+            {"Кількість ядер:", pe.NumberOfCores.HasValue ? pe.NumberOfCores.ToString() : "-"},
+            {"Частота, GHz:", pe.Clock.HasValue ? pe.Clock.ToString() : "-"},
+            {"Внутрішня пам'ять:", pe.Rom.HasValue ? $"{pe.Rom} ГБ":"-"},
+            {"Оперативна пам'ять:",pe.Ram.HasValue ? $"{pe.Ram} ГБ":"-"},
+            {"Ємність акумулятора:",pe.BatteryCapacity.HasValue ? $"{pe.BatteryCapacity} мАгод" : "-"},
+            {"Фронтальна камера:",!string.IsNullOrEmpty(pe.FrontCamera) ? pe.FrontCamera : "-"},
+            {"Основна камера:",!string.IsNullOrEmpty(pe.RearCamera) ? pe.RearCamera : "-"},
+            {"Ширина корпусу, мм:", pe.Width.HasValue ? pe.Width.ToString() : "-"},
+            {"Висота корпусу, мм:", pe.Height.HasValue ? pe.Height.ToString() : "-"},
+            {"Вага корпусу, г:", pe.Weight.HasValue ? pe.Weight.ToString() : "-"},
+        };
         private static string ExistingName(string name)
         {
             return string.IsNullOrEmpty(name) ? "" : $" {name}";
--- EShop/Pages/Customer/Product.cshtml.cs
+++ EShop/Pages/Customer/Product.cshtml.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EShop.Data;
 using EShop.Data.Entities;
+using EShop.Data.Helpers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace EShop.Pages.Customer
@@ -12,28 +16,63 @@ namespace EShop.Pages.Customer
     public class Product : PageModel
     {
         private readonly ShopContext _shopContext;
-
-        public Product(ShopContext shopContext)
+        private readonly UserManager<ApplicationUser> _userManager;
+        
+        public Product(ShopContext shopContext, UserManager<ApplicationUser> userManager)
         {
             _shopContext = shopContext;
+            _userManager = userManager;
         }
 
+        public Dictionary<string, string> SpecificationsList { get; set; }
+        
         public string Specifications { get; set; }
         public ProductEntity ProductEntity { get; set; }
+        public string Availability { get; set; }
         public async Task<IActionResult> OnGet(string id)
         {
             ProductEntity = await _shopContext.Products
                 .AsNoTracking()
                 .Include(x => x.Images)
                 .Where(x => x.Id == Guid.Parse(id)).FirstOrDefaultAsync();
+            SpecificationsList = TextHelper.PrepareSpecifications(ProductEntity);
             return Page();
         }
-
-        private string SetSpecifications()
+        public async Task<IActionResult> OnPostAddToCart(Guid? id)
         {
-            if (ProductEntity == null) return string.Empty;
-            
-            return "";
+            var userId = _userManager.GetUserId(Request.HttpContext.User);
+            if (string.IsNullOrEmpty(userId))
+            {
+                return RedirectToPage("/Account/Login");
+            }
+
+            var product = await _shopContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+            var existingShoppingCart =
+                await _shopContext.ShoppingCarts.Include(x => x.CartItems).ThenInclude(x => x.Item)
+                    .ThenInclude(x => x.Images).FirstOrDefaultAsync(
+                        x =>
+                            x.CustomerId == userId && !x.OrderId.HasValue);
+            var existingCartItem = existingShoppingCart.CartItems?.FirstOrDefault(x => x.ItemId == id);
+            if (existingCartItem != null)
+            {
+                existingCartItem.Quantity++;
+                _shopContext.CartItems.Update(existingCartItem);
+                await _shopContext.SaveChangesAsync();
+                return RedirectToPage("./Product", new {id});
+            }
+
+            if (existingShoppingCart.CartItems == null)
+            {
+                existingShoppingCart.CartItems = new List<CartItemEntity>();
+            }
+            existingShoppingCart.CartItems.Add(new CartItemEntity()
+            {
+                Item = product,
+                Quantity = 1
+            });
+            _shopContext.ShoppingCarts.Update(existingShoppingCart);
+            await _shopContext.SaveChangesAsync();
+            return RedirectToPage("./Product", new {id});
         }
     }
 }
